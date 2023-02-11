10ed4f0d7ae373a85aa18e4fbf0dd39a8846ce45
AdrIan2831 <112860695+AdrIan2831@users.noreply.github.com>
2023-02-11 14:25:34 +0300

    Add files via upload

---
+++ Горбуля Адраин 7 практическая работа н3/Горбуля Адраин 7 практическая работа н3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+public class ProcessFile
+{
+    public static void Main()
+    {
+        FileStream fs;
+        try
+        {
+            // Opens a text tile.
+            fs = new FileStream(@"C:\Users\STUDENT-4310\source\repos\Горбуля Адраин 7 практическая работа н3\Горбуля Адраин 7 практическая работа н3\bin\Debug\net5.0\ref\data.txt", FileMode.Open);
+            var sr = new StreamReader(fs);
+
+            // A value is read from the file and output to the console.
+            string line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine($"[Data File Missing] {e}");
+            throw new FileNotFoundException(@"[data.txt not in c:\temp directory]", e);
+        }
+        finally
+        {
+            fs = new FileStream(@"C:\Users\STUDENT-4310\source\repos\Горбуля Адраин 7 практическая работа н3\Горбуля Адраин 7 практическая работа н3\bin\Debug\net5.0\ref\data.txt", FileMode.Open);
+            if (fs != null)
+                fs.Close();
+        }
+    }
+}
